21e86087db709406548d4a8e10b919dde701edaf
VictoriaLasso <cajavi90@hotmail.com>
2016-10-31 10:10:03 -0500

    comillas
    se modifican las comillas para realizar pruebas

--- vcorr/generate_guis.py
+++ vcorr/generate_guis.py
@@ -3,7 +3,7 @@ __author__ = 'Diego'
 import os
 import PyQt4.uic
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     this_dir=os.path.dirname(__file__)
     qt_gui_dir=os.path.join(this_dir,'gui')
 
